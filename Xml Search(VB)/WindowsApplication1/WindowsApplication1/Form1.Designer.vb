e9cec82562b404396028d4752404b98cdbba5219
Kinesys <67867993+Kinesys@users.noreply.github.com>
2021-01-25 02:28:22 +0900

    Add files via upload

---
+++ Xml Search(VB)/WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기를 사용하여 수정하지 마십시오.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.Label = New System.Windows.Forms.Label()
+        Me.lvFile = New System.Windows.Forms.ListView()
+        Me.SuspendLayout()
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(197, 88)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearch.TabIndex = 0
+        Me.btnSearch.Text = "검색"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'Label
+        '
+        Me.Label.AutoSize = True
+        Me.Label.Location = New System.Drawing.Point(12, 14)
+        Me.Label.Name = "Label"
+        Me.Label.Size = New System.Drawing.Size(29, 12)
+        Me.Label.TabIndex = 1
+        Me.Label.Text = "이름"
+        '
+        'lvFile
+        '
+        Me.lvFile.GridLines = True
+        Me.lvFile.Location = New System.Drawing.Point(12, 29)
+        Me.lvFile.Name = "lvFile"
+        Me.lvFile.Size = New System.Drawing.Size(176, 220)
+        Me.lvFile.TabIndex = 2
+        Me.lvFile.UseCompatibleStateImageBehavior = False
+        Me.lvFile.View = System.Windows.Forms.View.Details
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.lvFile)
+        Me.Controls.Add(Me.Label)
+        Me.Controls.Add(Me.btnSearch)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "XML 파일 검색"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnSearch As System.Windows.Forms.Button
+    Friend WithEvents Label As System.Windows.Forms.Label
+    Friend WithEvents lvFile As System.Windows.Forms.ListView
+
+End Class
---
+++ Xml Search(VB)/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,58 @@
+﻿Imports System.Net 'WebClient 클래스 사용
+Imports System.Xml 'XmlDocument, XmlNodeList 클래스 사용
+Imports System.IO 'StringReader 클래스 사아
+
+Public Class Form1
+
+    Const headerURL = "https://localhost/"
+
+    Dim Nullable As XmlAttribute
+
+    Private Sub bthSearch_Click(ByVal sender As System.Object, _
+            ByVal e As System.EventArgs) Handles btnSearch.Click
+        Me.lvFile.Items.Clear()
+
+        Dim wc = New WebClient()
+
+        Dim buffer = wc.DownloadString(String.Format( _
+            " {0}WebXml.xml ", headerURL))
+
+        wc.Dispose()
+
+        Dim sr As StringReader = New StringReader(buffer)
+
+        Dim doc As XmlDocument = New XmlDocument()
+
+        doc.Load(sr)
+
+        sr.Close()
+
+        Dim forecastNodes As XmlNodeList = _
+            doc.SelectNodes(" xml_reply/human/human_entry ")
+
+        For Each node As XmlNode In forecastNodes
+            Me.lvFile.Items.Add(New ListViewItem( _
+                New String() {GetNodeValue(node, "title")}))
+        Next
+
+    End Sub
+
+    Private Function GetNodeValue(ByVal parent As XmlNode, _
+            ByVal name As String) As String
+
+        Try
+
+            Dim attr As XmlAttribute = _
+                parent.SelectSingleNode(name).Attributes("name")
+            If attr.Value <> " " Then
+                Return attr.Value
+            End If
+
+            Return " "
+        Catch
+            Return " "
+        End Try
+
+    End Function
+
+End Class
